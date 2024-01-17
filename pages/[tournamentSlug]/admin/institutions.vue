1a8b39d8d3e878ef1bf24447e3f8b3c5035dbba9
Tran Trang Linh <trantranglinh231104@gmail.com>
2024-01-17 04:38:16 -0500

    Add institution page

---
+++ pages/[tournamentSlug]/admin/institutions.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useTournamentsStore } from '~/stores/tournaments';
+
+definePageMeta({
+  name: 'tournament.admin.participants.institutions',
+});
+
+const tournamentsStore = useTournamentsStore();
+tournamentsStore.getInstitutions();
+tournamentsStore.getTeams();
+tournamentsStore.getAdjudicators();
+
+function getInstitutionName(institution){
+  return institution.name;
+}
+
+function getInstitutionCode(institution){
+  return institution.code
+}
+
+function getInstitutionRegion(institution){
+  return institution.region
+}
+
+const instMap = computed(() =>
+  Object.fromEntries(
+    tournamentsStore.institutions.map((inst) => [inst.url, inst]),
+  ),
+);
+
+function getInstitutionTeams(institution){
+  let number = 0;
+  for (var team of tournamentsStore.currentTournament.teams){
+    if (instMap.value[team.institution]?.code == getInstitutionCode(institution)) {
+      number++;
+    }
+  }
+  return number;
+}
+
+function getInstitutionAdjudicators(institution){
+  let number = 0;
+  for (var adj of tournamentsStore.currentTournament.adjudicators){
+    if (instMap.value[adj.institution]?.code == getInstitutionCode(institution)) {
+      number++;
+    }
+  }
+  return number;
+}
+
+const institutionsTable = computed(() => ({
+  headers: [
+    { title: 'Codename' },
+    { title: 'Institution' },
+    { title: 'Region' },
+    { title: 'Teams' },
+    { title: 'Adjudicators' },
+  ],
+  rows:
+    tournamentsStore.institutions?.map((inst) => ({
+      content: [
+        { value: getInstitutionCode(inst) },
+        { value: getInstitutionName(inst) },
+        { value: getInstitutionRegion(inst) },
+        { value: getInstitutionTeams(inst) },
+        { value: getInstitutionAdjudicators(inst) },
+      ],
+      subrows: [],
+      key: inst.url,
+      institution: inst,
+    })) ?? [],
+}));
+
+</script>
+
+<template>
+    <LayoutsAdmin>
+    <PageTitle emoji="🏫">Institutions
+      <template #nav>
+          <NuxtLink class="btn outline-primary" to="./participants">Participants</NuxtLink>
+          <NuxtLink class="btn outline-primary">Speaker Categories</NuxtLink>
+          <NuxtLink class="btn outline-primary">Private URLs</NuxtLink>
+        </template>
+    </PageTitle>
+    <div class="tables">
+      <TableBase
+        title="Institutions"
+        :content="institutionsTable"
+      >
+      </TableBase>
+    </div>
+  </LayoutsAdmin>
+</template>
