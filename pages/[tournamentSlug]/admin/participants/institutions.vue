23fab30872caad177ce331f048eb88d3fe1dc3d5
Tran Trang Linh <linh.tran@hws.edu>
2024-02-12 09:09:00 -0500

    Insert the search bar to pages

--- pages/[tournamentSlug]/admin/participants/institutions.vue
+++ pages/[tournamentSlug]/admin/participants/institutions.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import { FilterMatchMode } from 'primevue/api';
 import { storeToRefs } from 'pinia';
 import { useTournamentsStore } from '~/stores/tournaments';
 
@@ -39,6 +41,10 @@ const dt = ref();
 function exportCSV(event) {
   dt.value.exportCSV();
 }
+
+const institutionfilters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
 </script>
 
 <template>
@@ -74,10 +80,13 @@ function exportCSV(event) {
           :value="institutions"
           sort-mode="multiple"
           :loading="loading.teams !== false || loading.adjudicators !== false"
+          v-model:filters="institutionfilters"
+          :globalFilterFields="['name', 'code', 'region']"
         >
           <template #header>
             <div class="title">
               <h3>Institutions</h3>
+              <InputText v-model="institutionfilters['global'].value" placeholder="Search Institutions" class="searchbar" />
               <button v-tooltip="'Save as CSV'" class="btn info small" @click="exportCSV($event)">
                 <Icon type="Clipboard" size="22" />
               </button>
--- pages/[tournamentSlug]/admin/participants/privateurls.vue
+++ pages/[tournamentSlug]/admin/participants/privateurls.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import { FilterMatchMode } from 'primevue/api';
 import { storeToRefs } from 'pinia';
 import { useTournamentsStore } from '~/stores/tournaments';
 
@@ -32,6 +34,12 @@ const speakerTable = computed(
       urlKey: spk.urlKey,
     })),
 );
+const speakerfilters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+const adjudicatorfilters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
 </script>
 
 <template>
@@ -67,10 +75,13 @@ const speakerTable = computed(
           :value="adjTable"
           sort-mode="multiple"
           :loading="loading.adjudicators !== false"
+          v-model:filters="adjudicatorfilters"
+          :globalFilterFields="['name', 'urlKey']"
         >
           <template #header>
             <div class="title">
               <h3>Adjudicators</h3>
+              <InputText v-model="adjudicatorfilters['global'].value" placeholder="Search Adjudicators" class="searchbar small" />
               <button v-tooltip="'Save as CSV'" class="btn info small" @click="exportCSV($event)">
                 <Icon type="Clipboard" size="22" />
               </button>
@@ -87,7 +98,7 @@ const speakerTable = computed(
               <TableAdjudicatorCell :adjudicator="data.obj" />
             </template>
           </Column>
-          <Column field="urlKey" sortable>
+          <Column field="urlKey" sortable style="width: 10em">
             <template #header>
               <Icon v-tooltip="'URL Key'" type="Key" size="18" />
             </template>
@@ -103,10 +114,13 @@ const speakerTable = computed(
           :value="speakerTable"
           sort-mode="multiple"
           :loading="loading.speakers !== false"
+          v-model:filters="speakerfilters"
+          :globalFilterFields="['name', 'urlKey']"
         >
           <template #header>
             <div class="title">
               <h3>Speakers</h3>
+              <InputText v-model="speakerfilters['global'].value" placeholder="Search Speakers" class="searchbar small" />
               <button v-tooltip="'Save as CSV'" class="btn info small" @click="exportCSV($event)">
                 <Icon type="Clipboard" size="22" />
               </button>
@@ -126,7 +140,7 @@ const speakerTable = computed(
               <template v-else>{{ data.obj.name }}</template>
             </template>
           </Column>
-          <Column field="urlKey" sortable>
+          <Column field="urlKey" sortable style="width: 10em">
             <template #header>
               <Icon v-tooltip="'URL Key'" type="Key" size="18" />
             </template>
